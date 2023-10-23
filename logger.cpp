5ece88a658dee8a9667446f1ab5dca35d6dee5d6
NeAleks18 <nealeks18@mail.ru>
2023-10-23 12:36:37 +0900

    Patch to Linux

--- logger.cpp
+++ logger.cpp
@@ -1,29 +1,59 @@
-#include "logger.h"
-#include "colorswin.h"
+﻿#include "logger.h"
 #include <fstream>
 #include <ctime>
 
+#ifdef __linux__
+// Код для Linux
+#else
+#include "colorswin.h"
 Colors color;
+#endif
+
 const std::string FILENAME = ".log";
 
 void Logger::Log(const std::string& message) {
+#ifdef __linux__
+    // Код для Linux
+#else
     color.set_color(CL_WHITE);
+#endif
+
     std::cout << "[Log] " << message << std::endl;
     SaveToFile("[Log] " + message);
 }
 
 void Logger::Error(const std::string& message) {
+#ifdef __linux__
+    // Код для Linux
+#else
     color.set_color(CL_RED);
+#endif
+
     std::cerr << "[Error] " << message << std::endl;
     SaveToFile("[Error] " + message);
+
+#ifdef __linux__
+    // Код для Linux
+#else
     color.set_color(CL_WHITE);
+#endif
 }
 
 void Logger::Warning(const std::string& message) {
+#ifdef __linux__
+    // Код для Linux
+#else
     color.set_color(CL_YELLOW);
+#endif
+
     std::cout << "[Warning] " << message << std::endl;
     SaveToFile("[Warning] " + message);
+
+#ifdef __linux__
+    // Код для Linux
+#else
     color.set_color(CL_WHITE);
+#endif
 }
 
 void Logger::SaveToFile(const std::string& message) {
